877c45ad03e8d4a8f3d381c1bcb3680644f8fd90
John Rucker <JHRucker@outlook.com>
2020-09-23 10:52:49 -0500

    .

--- testMe.js
+++ testMe.js
@@ -1,14 +1,25 @@
 const WAPI = require('./weatherApiDataGetter');
 const actObj = require('./actObj.json');
 
-const wApi = new WAPI(actObj.apiKey, actObj.zipcode, true);
+const wApi = new WAPI(actObj.apiKey, actObj.zipcode, false);
 console.log('Requesting forecast for zipcode = ' + actObj.zipcode);
 wApi.getForecast()
     .then((rslt) => {
-        console.log('Forecast received now getting current conditions');
-        return wApi.getCurrent()
+        console.log('Parsed weather data follows:');
+        console.dir(wApi.data, {depth:null})
     })
     .catch((err) => {
         console.error('Error getting forecast from weatherApiDataGetter', err);
 
     });
+
+setInterval(()=>{
+    wApi.getForecast()
+    .then((rslt) => {
+        console.dir(wApi.data, {depth:null})
+    })
+    .catch((err) => {
+        console.error('Error getting forecast from weatherApiDataGetter', err);
+
+    });
+},60000 * 15)
--- weatherApiDataGetter.js
+++ weatherApiDataGetter.js
@@ -2,7 +2,28 @@ const EventEmitter = require('events');
 const fetch = require('node-fetch');
 
 const logPrefix = 'weatherApiDataGetter.js | ';
-const baseApiURL = 'https://api.weatherapi.com/v1'
+const baseApiURL = 'https://api.weatherapi.com/v1';
+const weatherDataObj = {
+    obsDate: undefined,
+    current:{
+        temp:undefined,
+        feelsLike:undefined,
+        wind:undefined,
+        windGust:undefined,
+        windDegree:undefined,
+        pressure:undefined,
+        precip:undefined,
+        humidity:undefined
+    },
+    forecast:{
+        maxTemp:undefined,
+        minTemp:undefined,
+        maxWind:undefined,
+        totalPrecip:undefined,
+        rainChance:undefined,
+        snowChance:undefined
+    }
+}
 
 class weatherApiDataGetter extends EventEmitter {
 
@@ -11,6 +32,7 @@ class weatherApiDataGetter extends EventEmitter {
         this.apiKey = apiKey;
         this.zipcode = zipcode;
         this.verbose = verboseLogging;
+        this.data = weatherDataObj;
     };
 
     getForecast() {
@@ -24,12 +46,33 @@ class weatherApiDataGetter extends EventEmitter {
             let uri = baseApiURL + '/forecast.json?key=' + this.apiKey + '&q=' + this.zipcode
             fetch(uri, callObj)
                 .then(res => res.json())
-                .then(json => {
+                .then(jsonData => {
                     if (this.verbose) {
                         logit('Weather Forecast follows:');
-                        console.dir(json, { depth: null });
+                        console.dir(jsonData, { depth: null });
                     }
-                    resolve(json);
+                    try{
+                    this.data.obsDate = jsonData.current.last_updated;
+                    this.data.current.temp = jsonData.current.temp_f;
+                    this.data.current.feelsLike = jsonData.current.feelslike_f;
+                    this.data.current.wind = jsonData.current.wind_mph;
+                    this.data.current.windDegree = jsonData.current.wind_degree;
+                    this.data.current.windGust = jsonData.current.gust_mph;
+                    this.data.current.pressure = jsonData.current.pressure_in;
+                    this.data.current.precip = jsonData.current.precip_in;
+                    this.data.current.humidity = jsonData.current.humidity;
+
+                    this.data.forecast.maxTemp = jsonData.forecast.forecastday[0].day.maxtemp_f;
+                    this.data.forecast.minTemp = jsonData.forecast.forecastday[0].day.mintemp_f;
+                    this.data.forecast.maxWind = jsonData.forecast.forecastday[0].day.maxwind_mph;
+                    this.data.forecast.totalPrecip = jsonData.forecast.forecastday[0].day.totalprecip_in;
+                    this.data.forecast.rainChance = jsonData.forecast.forecastday[0].day.daily_chance_of_rain;
+                    this.data.forecast.snowChance = jsonData.forecast.forecastday[0].day.daily_chance_of_snow;
+                    resolve(jsonData);
+                    } catch (err) {
+                        logit('error parasing weather data:' + err);
+                        reject(err);
+                    }
                 })
                 .catch(err => {
                     console.error('Error calling ' + uri, err);
